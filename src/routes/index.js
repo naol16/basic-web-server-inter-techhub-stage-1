ccdd5a8594342e2d65b9d132fea51a0b23c22b21
naol16 <lemesanaol93@gmail.com>
2024-11-18 19:14:58 +0300

    worked the three servers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,26 @@
 const express= require('express')
 const app= express()
 const port =process.env.port || 3412 ;
-
-app.get('/',(req,res)=>{
-    res.send("naola");
+app.get("/",(req,res)=>{
+    res.send("welcome");
 })
+app.get('/name',(req,res)=>{
+    res.send("Naol Lemessa Ayana");
+})
+app.get('/hobby',(req,res)=>{
+    // my  hobbey object
+    const funnymoment={
+        reading:["tuseday with morey","atomic habit","lon walk to free dom"],
+        football:["Arsenal","barcelona"],
+        sport:["football","basketball"]
+    }
+    res.json(funnymoment);
+
+})
+app.get('/dream',(req,res)=>{
+    res.send("the only way out is through")
+})
+
 app.listen(port,(err)=>{
     if (err){
         console.log("error");
